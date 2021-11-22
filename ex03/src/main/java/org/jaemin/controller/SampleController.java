b4134cc8a204cc5ad8ca7342ed07a7a7cbf5dca6
VvsJaemin <djd453216@nate.com>
2021-11-23 00:09:34 +0900

    211122 RestController 호출 방식

--- ex03/src/main/java/org/jaemin/controller/SampleController.java
+++ ex03/src/main/java/org/jaemin/controller/SampleController.java
@@ -1,5 +1,11 @@
 package org.jaemin.controller;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.jaemin.domain.SampleVO;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +18,21 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class SampleController {
 	
-	@GetMapping(value = "/getText", produces = "text/plain; chartset=UTF-8")
+	@GetMapping(value = "/getText", produces = "text/plain; charset=UTF-8")
 	public String getText() {
 		log.info("MIME TYPE : " + MediaType.TEXT_PLAIN_VALUE);
 		
 		return "안녕하세요";
+	}
+	
+	@GetMapping(value = "/getSample", produces= { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+	public SampleVO getSample() {
+		return new SampleVO(113, "로켓", "라쿤");
+	}
+	
+	@GetMapping(value = "/getList")
+	public List<SampleVO> getList(){
+		
+		return IntStream.range(1, 10).mapToObj(i->new SampleVO(i, i+"First", i+"Last")).collect(Collectors.toList());
 	}
 }
---
+++ ex03/src/main/java/org/jaemin/domain/SampleVO.java
@@ -0,0 +1,15 @@
+package org.jaemin.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SampleVO {
+	
+	private int mno;
+	private String firstName;
+	private String lastName;
+}
