47179c221031ba74afec5edde9a36c73fe7d6ccc
VvsJaemin <djd453216@nate.com>
2021-11-27 00:19:28 +0900

    211127 reply controller

---
+++ ex03/src/main/java/org/jaemin/controller/ReplyController.java
@@ -0,0 +1,70 @@
+package org.jaemin.controller;
+
+import java.util.List;
+
+import org.jaemin.domain.Criteria;
+import org.jaemin.domain.ReplyVO;
+import org.jaemin.service.ReplyService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import oracle.jdbc.proxy.annotation.Post;
+
+@RequestMapping("/replies")
+@RestController
+@Log4j
+@AllArgsConstructor
+public class ReplyController {
+
+	private ReplyService service;
+
+	@PostMapping(value = "/new", consumes = "application/json", produces = { MediaType.TEXT_PLAIN_VALUE })
+	public ResponseEntity<String> create(@RequestBody ReplyVO vo) {
+
+		int insertCount = service.register(vo);
+
+		return insertCount == 1 ? new ResponseEntity<>("success", HttpStatus.OK)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+	@GetMapping(value = "/pages/{bno}/{page}", produces = { MediaType.APPLICATION_XML_VALUE,
+			MediaType.APPLICATION_JSON_UTF8_VALUE })
+	public ResponseEntity<List<ReplyVO>> getList(@PathVariable("page") int page, @PathVariable("bno") Long bno) {
+		Criteria cri = new Criteria(page, 10);
+
+		return new ResponseEntity<>(service.getList(cri, bno), HttpStatus.OK);
+	}
+
+	@GetMapping(value = "/{rno}")
+	public ResponseEntity<ReplyVO> get(@PathVariable("rno") Long rno) {
+		return new ResponseEntity<>(service.get(rno), HttpStatus.OK);
+	}
+
+	@DeleteMapping(value = "/{rno}")
+	public ResponseEntity<String> remove(@PathVariable("rno") Long rno) {
+
+		return service.remove(rno) == 1 ? new ResponseEntity<>("success", HttpStatus.OK)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+	
+	@PutMapping(value = "/{rno}", consumes = "application/json")
+	public ResponseEntity<String>modify(@RequestBody ReplyVO vo, @PathVariable("rno")Long rno){
+		vo.setRno(rno);
+		
+		return service.update(vo)==1 ? new ResponseEntity<>("success", HttpStatus.OK)
+				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+
+}
