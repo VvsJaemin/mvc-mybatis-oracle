94eadfec26bae72db8c59522e4c5ad835f9febc1
VvsJaemin <djd453216@nate.com>
2021-12-01 23:50:58 +0900

    211201 AOP Complete

--- ex04/src/main/java/org/jaemin/aop/LogAdvice.java
+++ ex04/src/main/java/org/jaemin/aop/LogAdvice.java
@@ -1,5 +1,10 @@
 package org.jaemin.aop;
 
+import java.util.Arrays;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -14,6 +19,42 @@ public class LogAdvice {
 	@Before( "execution(* org.jaemin.service.SampleService*.*(..))")
 	public void logBefore() {
 		log.info("===============================");
+	}
+	
+	@Before( "execution(* org.jaemin.service.SampleService*.doAdd(String, String)) && args(str1, str2)")
+	public void logBeforeParam(String str1, String str2) {
+		log.info("Str1" + str1);
+		log.info("Str2" + str2);
+	}
+	
+	@AfterThrowing(pointcut = "execution(* org.jaemin.service.SampleService*.*(..))", throwing = "exception")
+	public void logException(Exception exception) {
+		log.info("exception!!!");
+		log.info("exception : " + exception);
+	}
+	
+	@Around("execution(* org.jaemin.service.SampleService*.*(..))")
+	public Object logTime(ProceedingJoinPoint pjp) {
+		long start = System.currentTimeMillis();
+		
+		log.info("Target : " + pjp.getTarget());
+		log.info("Param : " + Arrays.toString(pjp.getArgs()));
+		
+		//invoke method
+		Object result = null;
+		
+		try {
+			result = pjp.proceed();
+		}catch (Throwable e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		long end = System.currentTimeMillis();
+		
+		log.info("Time : " + (end-start));
+		
+		return result;
 	}
 	
 }
